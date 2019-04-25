e9cd8bc698ebdb0fa92e1afba71fbbdaa0887673
Gabriel Levi <gabrielevigomes@gmail.com>
2019-04-24 21:21:53 -0300

    Fix bug on camera.

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -24,19 +24,19 @@ void Camera::Update(float dt) {
     }
     else {
         if (input.IsKeyDown(UP_ARROW_KEY)) {
-            speed = {0, -dt*250};
-            pos = pos + speed;
-        }
-        else if (input.IsKeyDown(DOWN_ARROW_KEY)) {
             speed = {0, dt*250};
             pos = pos + speed;
         }
+        else if (input.IsKeyDown(DOWN_ARROW_KEY)) {
+            speed = {0, -dt*250};
+            pos = pos + speed;
+        }
         else if (input.IsKeyDown(RIGHT_ARROW_KEY)) {
-            speed = {dt*250, 0};
+            speed = {-dt*250, 0};
             pos = pos + speed;
         }
         else if (input.IsKeyDown(LEFT_ARROW_KEY)) {
-            speed = {-dt*250, 0};
+            speed = {dt*250, 0};
             pos = pos + speed;
         }
     }
